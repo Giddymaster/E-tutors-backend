217beca3fece44e1f3d10bd534be58f5eeb99464
unknown <intjgmwas7@gmail.com>
2025-12-31 21:13:16 -0800

    Remove node_modules, dist, and .env from tracking

--- dist/routes/student.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const students_controller_1 = require("../controllers/students.controller");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const router = (0, express_1.Router)();
-router.get('/me', auth_middleware_1.authenticate, students_controller_1.getMyStudent);
-router.patch('/me', auth_middleware_1.authenticate, students_controller_1.updateMyStudent);
-exports.default = router;
